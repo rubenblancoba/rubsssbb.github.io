3dc1ae5c9dee590e3adc72c37c2e76dc43cdd461
rubsssbb <rubenblancob98@gmail.com>
2020-10-22 21:09:29 +0200

    Fijado top

--- myScript.js
+++ myScript.js
@@ -17,6 +17,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+//lugar
 function displayPlace(){
   document.getElementById("Lugar").innerHTML="Madrid"
 }
